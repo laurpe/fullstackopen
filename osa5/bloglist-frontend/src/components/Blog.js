8d9a1bd2d8f892fec227776e860e3240d3100204
Laura Perttilä <laura@perttila.me>
2020-12-08 19:26:02 +0200

    tehtävä 5.7

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+const Blog = ({ blog }) => {
+  const [blogDetails, setBlogDetails] = useState(false)
+
+  const hideDetails = { display: blogDetails ? 'none' : '' }
+  const showDetails = { display: blogDetails ? '' : 'none' }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 5,
+    border: 'solid',
+    borderWidth: 1,
+    marginTop: 2,
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div style={hideDetails}>
+        {blog.title} by {blog.author} <button onClick={() => setBlogDetails(true)}>view</button>
+      </div>
+      <div style={showDetails}>
+        {blog.title} by {blog.author} <button onClick={() => setBlogDetails(false)}>hide</button> <br />
+        {blog.url} <br />
+        likes: {blog.likes} <button>like</button><br />
+        {blog.user.name}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
