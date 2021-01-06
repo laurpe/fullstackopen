09118e7b6e2040030ebdea53d4371b19f736f416
Laura Perttilä <laura@perttila.me>
2021-01-06 09:23:02 +0200

    tehtävä 5.18

--- osa5/bloglist-frontend/cypress/integration/bloglist_app.spec.js
+++ osa5/bloglist-frontend/cypress/integration/bloglist_app.spec.js
@@ -1,6 +1,12 @@
 describe('Blog app', function() {
     beforeEach(function() {
         cy.request('POST', 'http://localhost:3001/api/testing/reset')
+        const user = {
+            name: 'Testi Testaaja',
+            username: 'testeri',
+            password: 'salasana'
+        }
+        cy.request('POST', 'http://localhost:3001/api/users', user)
         cy.visit('http://localhost:3000')
     })
 
@@ -9,5 +15,22 @@ describe('Blog app', function() {
         cy.contains('username')
         cy.contains('password')
         cy.contains('login')
+    })
+
+    describe('Login', function() {
+        it('succeeds with correct credentials', function() {
+            cy.get('#username').type('testeri')
+            cy.get('#password').type('salasana')
+            cy.get('#login-button').click()
+            cy.contains('Testi Testaaja')
+        })
+
+        it('fails with wrong credentials', function() {
+            cy.get('#username').type('testeri')
+            cy.get('#password').type('salainen')
+            cy.get('#login-button').click()
+            cy.get('.error').contains('wrong username or password')
+            cy.get('.error').should('have.css', 'background-color', 'rgb(206, 57, 77)')
+        })
     })
 })
