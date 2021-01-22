92535d6e80a3f8cff091177220f42487d7531e85
Laura Perttilä <laura@perttila.me>
2021-01-22 11:21:10 +0200

    tehtävä 6.9

---
+++ osa6/redux-anecdotes/src/store.js
@@ -0,0 +1,7 @@
+import reducer from './reducers/anecdoteReducer'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { createStore } from 'redux'
+
+const store = createStore(reducer, composeWithDevTools())
+
+export default store
