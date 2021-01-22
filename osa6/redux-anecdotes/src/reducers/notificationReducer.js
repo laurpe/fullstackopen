85076fdede8549f8e884982efdd5be5254beecd9
Laura Perttilä <laura@perttila.me>
2021-01-22 12:05:15 +0200

    tehtävä 6.10

---
+++ osa6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,19 @@
+
+
+const notificationReducer = (state = 'default message', action) => {
+    switch (action.type) {
+        case 'SET_NOTIFICATION':
+            return action.notification
+        default:
+            return state
+    }
+}
+
+export const setNotification = (notification) => {
+    return {
+        type: 'SET_NOTIFICATION',
+        notification
+    }
+}
+
+export default notificationReducer
--- osa6/redux-anecdotes/src/store.js
+++ osa6/redux-anecdotes/src/store.js
@@ -1,6 +1,13 @@
-import reducer from './reducers/anecdoteReducer'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
+
+import anecdoteReducer from './reducers/anecdoteReducer'
+import notificationReducer from './reducers/notificationReducer'
+
+const reducer = combineReducers({
+    anecdotes: anecdoteReducer,
+    notification: notificationReducer
+})
 
 const store = createStore(reducer, composeWithDevTools())
 
