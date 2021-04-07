f28fa699e8725f2322afed56c6d98a75d9c6b4d6
Laura Perttilä <laura@perttila.me>
2021-04-07 20:43:33 +0300

    tehtävä 8.2

--- osa8/library-backend/index.js
+++ osa8/library-backend/index.js
@@ -1,4 +1,4 @@
-const { ApolloServer, gql } = require('apollo-server')
+const { ApolloServer, gql, RenameRootFields } = require('apollo-server')
 
 let authors = [
     {
@@ -87,13 +87,21 @@ const typeDefs = gql`
   type Query {
       bookCount: Int!
       authorCount: Int!
+      allBooks: [Book!]!
+  }
+  type Book {
+      title: String!
+      published: Int!
+      author: String!
+      genres: [String!]!
   }
 `
 
 const resolvers = {
     Query: {
         bookCount: () => books.length,
-        authorCount: () => authors.length
+        authorCount: () => authors.length,
+        allBooks: () => books
     }
 }
 
