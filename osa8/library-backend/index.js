2f1dbf1498ee8cfd551183d36e6910a1dc8ab5d4
Laura Perttilä <laura@perttila.me>
2021-04-16 11:43:44 +0300

    tehtävä 8.5

--- osa8/library-backend/index.js
+++ osa8/library-backend/index.js
@@ -87,7 +87,7 @@ const typeDefs = gql`
   type Query {
       bookCount: Int!
       authorCount: Int!
-      allBooks(author: String): [Book!]!
+      allBooks(author: String, genre: String): [Book!]!
       allAuthors: [Author!]!
   }
   type Book {
@@ -108,10 +108,16 @@ const resolvers = {
         bookCount: () => books.length,
         authorCount: () => authors.length,
         allBooks: (root, args) => {
-            if (!args.author) {
-                return books
+            if (args.author && args.genre) {
+                return books.filter(book => book.genres.includes(args.genre) && book.author === args.author)
             }
-            return books.filter(book => book.author === args.author)
+            if (args.author) {
+                return books.filter(book => book.author === args.author)
+            }
+            if (args.genre) {
+                return books.filter(book => book.genres.includes(args.genre))
+            }
+            return books
         },
         allAuthors: () => authors
     },
