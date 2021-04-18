77316013d83146acfc18a5e0f3fa77265763ec05
Laura Perttilä <laura@perttila.me>
2021-04-18 17:10:36 +0300

    tehtävä 8.7

--- osa8/library-backend/index.js
+++ osa8/library-backend/index.js
@@ -111,6 +111,10 @@ const typeDefs = gql`
         published: Int!
         genres: [String!]
     ): Book
+    editAuthor(
+        name: String!
+        setBornTo: Int!
+    ): Author
   }
 `
 
@@ -150,6 +154,16 @@ const resolvers = {
             }
 
             return book
+        },
+        editAuthor: (root, args) => {
+            const author = authors.find(author => author.name === args.name)
+            if (!author) {
+                return null
+            }
+
+            const updatedAuthor = { ...author, born: args.setBornTo }
+            authors = authors.map(author => author.name === args.name ? updatedAuthor : author)
+            return updatedAuthor
         }
     }
 }
