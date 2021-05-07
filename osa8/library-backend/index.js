06b652eb479cd465139c875c86e51d013fcefab6
Laura Perttilä <laura@perttila.me>
2021-05-07 12:27:33 +0300

    tehtävä 8.16

--- osa8/library-backend/index.js
+++ osa8/library-backend/index.js
@@ -1,8 +1,12 @@
 require('dotenv').config()
-const { ApolloServer, gql, UserInputError } = require('apollo-server')
+const { ApolloServer, gql, UserInputError, AuthenticationError } = require('apollo-server')
+const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose')
 const Book = require('./models/book')
 const Author = require('./models/author')
+const User = require('./models/user')
+
+const JWT_SECRET = 'SECRET'
 
 const url = process.env.MONGODB_URI
 
@@ -13,6 +17,8 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFind
     .catch((error) => {
         console.log('error connection to MongoDB:', error.message)
     })
+
+
 
 
 let authors = [
@@ -172,7 +178,10 @@ const resolvers = {
             }
             return Book.find({})
         },
-        allAuthors: () => Author.find({})
+        allAuthors: () => Author.find({}),
+        me: (root, args, context) => {
+            return context.currentUser
+        }
     },
     Author: {
         bookCount: async (root) => {
@@ -182,7 +191,11 @@ const resolvers = {
         }
     },
     Mutation: {
-        addBook: async (root, args) => {
+        addBook: async (root, args, context) => {
+            if (!context.currentUser) {
+                throw new AuthenticationError("Invalid token")
+            }
+
             const author = await Author.findOne({ name: args.author })
 
             if (!author) {
@@ -199,26 +212,28 @@ const resolvers = {
                 const book = new Book({ ...args, author: newAuthor })
 
                 try {
-                    await book.save()
+                    return await book.save()
                 } catch (error) {
                     throw new UserInputError(error.message, {
                         invalidArgs: args
                     })
                 }
-                return book
             }
             const book = new Book({ ...args, author: author })
 
             try {
-                await book.save()
+                return await book.save()
             } catch (error) {
                 throw new UserInputError(error.message, {
                     invalidArgs: args
                 })
             }
-            return book
         },
-        editAuthor: async (root, args) => {
+        editAuthor: async (root, args, context) => {
+            if (!context.currentUser) {
+                throw new AuthenticationError("Invalid token")
+            }
+
             const author = await Author.findOne({ name: args.name })
 
             if (!author) {
@@ -228,14 +243,37 @@ const resolvers = {
             author.born = args.setBornTo
 
             try {
-                await author.save()
+                return await author.save()
             } catch (error) {
                 throw new UserInputError(error.message, {
                     invalidArgs: args
                 })
             }
+        },
+        createUser: async (root, args) => {
+            const user = new User({ username: args.username, favoriteGenre: args.favoriteGenre })
 
-            return author
+            try {
+                return await user.save()
+            } catch (error) {
+                throw new UserInputError(error.message, {
+                    invalidArgs: args
+                })
+            }
+        },
+        login: async (root, args) => {
+            const user = await User.findOne({ username: args.username })
+
+            if (!user || args.password !== 'secret') {
+                throw new UserInputError("wrong credentials")
+            }
+
+            const userForToken = {
+                username: user.username,
+                id: user._id
+            }
+
+            return { value: jwt.sign(userForToken, JWT_SECRET) }
         }
     }
 }
@@ -243,6 +281,19 @@ const resolvers = {
 const server = new ApolloServer({
     typeDefs,
     resolvers,
+    context: async ({ req }) => {
+        const auth = req ? req.headers.authorization : null
+
+        if (auth && auth.toLowerCase().startsWith('bearer ')) {
+            const decodedToken = jwt.verify(
+                auth.substring(7), JWT_SECRET
+            )
+            const currentUser = await User.findById(decodedToken.id)
+            console.log(currentUser)
+
+            return { currentUser }
+        }
+    }
 })
 
 server.listen().then(({ url }) => {
