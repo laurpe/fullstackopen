d57b15f1ee14920d545d0da2ce22713ad767229d
Laura Perttilä <laura@perttila.me>
2021-06-21 15:00:45 +0300

    exercise 9.9

--- osa9/patientor/index.ts
+++ osa9/patientor/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import cors = require('cors');
+
 const app = express();
+
+
 app.use(express.json());
+app.use(cors());
+const PORT = 3001;
 
-const PORT = 3000;
-
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
   console.log('pinged');
   res.send('pong');
 });
