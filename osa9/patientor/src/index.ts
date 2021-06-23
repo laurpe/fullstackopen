bb94be305bb565afb9a67fed177049a6b08226fd
Laura Perttilä <laura@perttila.me>
2021-06-23 10:05:25 +0300

    exercise 9.10

--- osa9/patientor/src/index.ts
+++ osa9/patientor/src/index.ts
@@ -1,17 +1,20 @@
 import express from 'express';
 import cors = require('cors');
+import diagnosesRouter from './routes/diagnoses';
 
 const app = express();
 
-
 app.use(express.json());
 app.use(cors());
+
 const PORT = 3001;
 
 app.get('/api/ping', (_req, res) => {
   console.log('pinged');
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosesRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
---
+++ osa9/patientor/src/services/diagnosesService.ts
@@ -0,0 +1,12 @@
+import diagnosesData from '../../data/diagnoses.json';
+import {Diagnosis} from '../types';
+
+const diagnoses: Array<Diagnosis> = diagnosesData;
+
+const getDiagnoses = (): Array<Diagnosis> => {
+  return diagnoses;
+};
+
+export default {
+  getDiagnoses
+};
